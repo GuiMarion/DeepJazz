f850ff9038b867ec8a177566cae3d44ed3aa8aaf
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-03-18 20:29:21 +0100

    dataset

--- PrintResults.py
+++ PrintResults.py
@@ -10,10 +10,7 @@ def main(name):
     seq = pickle.load(open("Results/"+name, 'rb'))
     seq = [np.transpose(seq, axes=(1, 0))]
     picklefile = "DeepBach/datasets/custom_dataset/"+ name + ".pickle"
-    X, X_metadatas, voice_ids, index2notes, note2indexes, metadatas = pickle.load(open(picklefile, 'rb'))
-
-    print(seq)
-
+    X, _, voice_ids, index2notes, note2indexes, _ = pickle.load(open(picklefile, 'rb'))
     for elem in seq:
         Compt = 0
 
@@ -22,29 +19,20 @@ def main(name):
                 Compt = 0
                 print()
                 for e in range(i-16, i):
+                    if e % 4 == 0:
+                        print(" ", end="")
                     print(index2notes[1][int(elem[1][e])], end="")
                 print()
-                for e in range(i-16, i):
-                    print(index2notes[2][int(elem[2][e])], end="")
                 print()
-                print()
-
+            if i % 4 == 0:
+                print(" ", end="")
             print(index2notes[0][int(elem[0][i])].replace("-","b"), end="")
             Compt+=1
         print()
         for e in range(len(elem[0])-16, len(elem[0])):
+            if e % 4 == 0:
+                print(" ", end="")
             print(index2notes[1][int(elem[1][e])], end="")
-        print()
-        for e in range(len(elem[0])-16, len(elem[0])):
-            print(index2notes[2][int(elem[2][e])], end="")
-        
-
-        print()
-        if (elem is not X[-1]):
-            print()
-            print("Another One")
-        print()
-
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
